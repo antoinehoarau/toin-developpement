9743ad6a54a02e80056cb392d1e4a0bbf929ebc8
Antoine <info@toindeveloppement.fr>
2022-03-26 16:15:55 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: "#FAFFFD",
+      black: "#080708",
+      gray: "#201E1F",
+      yellow: "#F8C630",
+      red: "#F40000",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
