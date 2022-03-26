50df5c91130814a06708b4cfa6d20db09cfc9eb7
Antoine <info@toindeveloppement.fr>
2022-03-26 16:19:18 +0400

    refractor : ajout de police

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
-      white: "#FAFFFD",
-      black: "#080708",
-      gray: "#201E1F",
-      yellow: "#F8C630",
-      red: "#F40000",
+      white: '#FAFFFD',
+      black: '#080708',
+      gray: '#201E1F',
+      yellow: '#F8C630',
+      red: '#F40000',
     },
     fontFamily: {
-      sans: ["Graphik", "sans-serif"],
+      nunito: ['Nunito sans', 'sans-serif'],
     },
     extend: {},
   },
   plugins: [],
-};
+}
