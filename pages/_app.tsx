bda3756b950183f529fbf2da595a9e3528dd1988
Antoine <info@toindeveloppement.fr>
2022-05-27 09:57:12 +0400

    tttt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
+/* eslint-disable @next/next/inline-script-id */
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Container from '../components/Container'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YDNNH0V5G9`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', G-YDNNH0V5G9);
+      `}
+      </Script>
+
       <Container>
         <Component {...pageProps} />
       </Container>
